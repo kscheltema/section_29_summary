c32f80fdbc362e1e074f76a8f0b2f03061ddb63b
kscheltema <k87scheltema@outlook.com>
2021-07-22 18:11:40 +0200

    more css classes

--- src/App.js
+++ src/App.js
@@ -4,8 +4,8 @@ function App() {
       <h1>My Todos</h1>
       <div className="card">
         <h2>TITLE</h2>
-        <div>
-          <button>Delete</button>
+        <div className="actions">
+          <button className="btn">Delete</button>
         </div>
       </div>
     </div>
