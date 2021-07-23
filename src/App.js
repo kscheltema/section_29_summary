6b3cf376b640c3fee44851dc6805c7dbb287f196
kscheltema <k87scheltema@outlook.com>
2021-07-23 15:49:54 +0200

    new app download Swatcha

--- src/App.js
+++ src/App.js
@@ -1,15 +1,5 @@
-import Todo from "./component/Todo";
-
 function App() {
-  return (
-    <div>
-      <h1>My Todos</h1>
-      <Todo title="first" />
-      <Todo title="second" />
-      <Todo title="third" />
-    </div>
-  );
+  return <div></div>;
 }
 
 export default App;
-//need to write code in child and parent component for individual attributes
--- src/index.js
+++ src/index.js
@@ -1,9 +1,7 @@
-import React from "react";
-import ReactDOM from "react-dom";
+import React from 'react';
+import ReactDOM from 'react-dom';
 
-import "./index.css";
-import App from "./App";
+import './index.css';
+import App from './App';
 
-ReactDOM.render(<App />, document.getElementById("root"));
-
-//index.js is the first code to be executed by the browser
+ReactDOM.render(<App />, document.getElementById('root'));
