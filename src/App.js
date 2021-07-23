299c75f513ec198141f91f727bac02f09c98c589
kscheltema <k87scheltema@outlook.com>
2021-07-23 12:30:00 +0200

    child and parent using props

--- src/App.js
+++ src/App.js
@@ -4,9 +4,12 @@ function App() {
   return (
     <div>
       <h1>My Todos</h1>
-      <Todo />
+      <Todo title="first" />
+      <Todo title="second" />
+      <Todo title="third" />
     </div>
   );
 }
 
 export default App;
+//need to write code in child and parent component for individual attributes
