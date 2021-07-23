f3666c34cb290a877cb80580d1b6f9fdc6f53f5c
kscheltema <k87scheltema@outlook.com>
2021-07-23 16:53:26 +0200

    basic NavBar

--- src/App.js
+++ src/App.js
@@ -2,15 +2,18 @@ import { Route, Switch } from "react-router-dom";
 import AllMeetupsPage from "./pages/AllMeetups";
 import FavoritesPage from "./pages/Favorites";
 import NewMeetupPage from "./pages/NewMeetup";
+import MainNav from "./components/Layout/MainNav";
 
 function App() {
   return (
     <div>
-      <Switch>
-        <Route path="/" component={AllMeetupsPage} exact />
-        <Route path="/new-meetup" component={NewMeetupPage} />
-        <Route path="/favorites" component={FavoritesPage} />
-      </Switch>
+      <MainNav>
+        <Switch>
+          <Route path="/" component={AllMeetupsPage} exact />
+          <Route path="/new-meetup" component={NewMeetupPage} />
+          <Route path="/favorites" component={FavoritesPage} />
+        </Switch>
+      </MainNav>
     </div>
   );
 }
