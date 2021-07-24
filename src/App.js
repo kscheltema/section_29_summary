59605f02222776659ad1aaff395d51043975b702
kscheltema <k87scheltema@outlook.com>
2021-07-24 09:18:35 +0200

    more wrapping component with props.children

--- src/App.js
+++ src/App.js
@@ -2,18 +2,17 @@ import { Route, Switch } from "react-router-dom";
 import AllMeetupsPage from "./pages/AllMeetups";
 import FavoritesPage from "./pages/Favorites";
 import NewMeetupPage from "./pages/NewMeetup";
-import MainNav from "./components/layout/MainNav";
+import Layout from "./components/layout/Layout";
 
 function App() {
   return (
-    <div>
-      <MainNav />
+    <Layout>
       <Switch>
         <Route path="/" component={AllMeetupsPage} exact />
         <Route path="/new-meetup" component={NewMeetupPage} />
         <Route path="/favorites" component={FavoritesPage} />
       </Switch>
-    </div>
+    </Layout>
   );
 }
 
