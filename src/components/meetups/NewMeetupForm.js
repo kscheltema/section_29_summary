f65a2a85b1244a1d648460b29aa9d335fec26329
kscheltema <k87scheltema@outlook.com>
2021-07-25 12:06:49 +0200

    basic POST request with FB

--- src/components/meetups/NewMeetupForm.js
+++ src/components/meetups/NewMeetupForm.js
@@ -2,7 +2,7 @@ import { useRef } from "react";
 import Card from "../ui/Card";
 import classes from "./NewMeetupForm.module.css";
 
-function NewMeetup() {
+function NewMeetup(props) {
   const titleInputRef = useRef();
   const imageInputRef = useRef();
   const addressInputRef = useRef();
@@ -21,7 +21,7 @@ function NewMeetup() {
       address: enteredAddress,
       description: enteredDescription,
     };
-    console.log(meetupData);
+    props.onAddMeetup(meetupData);
   }
 
   return (
--- src/pages/NewMeetup.js
+++ src/pages/NewMeetup.js
@@ -1,10 +1,23 @@
 import NewMeetupForm from "../components/meetups/NewMeetupForm";
 
 function NewMeetupPage() {
+  function onAddMeetupHandler(meetupData) {
+    fetch(
+      "https://burgerbuilder-89b34-default-rtdb.firebaseio.com/meetups.json",
+      {
+        method: "POST",
+        body: JSON.stringify(meetupData),
+        header: {
+          "Content-Type": "application/json",
+        },
+      }
+    ); //.json is a FB requirement
+  }
+
   return (
     <section>
       <h1>Add New Meetup</h1>
-      <NewMeetupForm />
+      <NewMeetupForm onAddMeetup={onAddMeetupHandler} />
     </section>
   );
 }
